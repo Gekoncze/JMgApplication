b6c61e54dd88f8084171266e90747cfd96684f97
Me <gekoncze@seznam.cz>
2021-01-23 12:49:41 +0100

    todo cleanup

--- src/cz/mg/application/architecture/MgDependency.java
+++ src/cz/mg/application/architecture/MgDependency.java
@@ -22,4 +22,8 @@ public class MgDependency extends MgStaticalEntity {
     public MgIdentity getIdentity() {
         return identity;
     }
+
+    public Map<Long, Long> getIdMap() {
+        return idMap;
+    }
 }
